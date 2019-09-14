62f53d4c00dc5318d5536166ab14152417a6589d
Nikki Consolacion <nikkiconsolacion@gmail.com>
2019-09-14 10:58:03 -0700

    got users and departments tables

--- db.js
+++ db.js
@@ -6,7 +6,27 @@ const client = new Client('postgres://localhost/the_people');
 
 client.connect();
 
+
+
 const SQL = `
+  DROP TABLE IF EXISTS users;
+  DROP TABLE IF EXISTS departments;
+  CREATE TABLE departments(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL
+  );
+  CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL
+  );
+  INSERT INTO departments(name) VALUES('hr');
+  INSERT INTO departments(name) VALUES('sales');
+  INSERT INTO departments(name) VALUES('marketing');
+  INSERT INTO departments(name) VALUES('it');
+  INSERT INTO departments(name) VALUES('users with no department');
+  INSERT INTO users(name) VALUES('moe');
+  INSERT INTO users(name) VALUES('larry');
+  INSERT INTO users(name) VALUES('curly');
 
 `;
 
@@ -14,14 +34,17 @@ const syncAndSeed = async()=> {
   await client.query(SQL);
 };
 
-const findAllUsers = ()=> {
-
+const findAllUsers = async()=> {
+  const response = await client.query('SELECT * FROM users;');
+  return response.rows;
 }
 
-const findAllDepartments = ()=> {
-
+const findAllDepartments = async()=> {
+  const response = await client.query('SELECT * FROM departments;');
+  return response.rows;
 }
 
+//syncAndSeed();
 module.exports = {
   syncAndSeed,
   findAllUsers,
